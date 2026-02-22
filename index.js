fc3dfa07a8b589fdbd5baf1b5768818dff490b2d
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2026-02-22 16:45:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 // app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
