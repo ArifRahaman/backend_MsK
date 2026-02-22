5efa662f2057298ae82b11e1603dd3c5b425c5d5
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2026-02-22 16:45:50 +0530

    Configure Cloudinary settings in index.js
    Add Cloudinary configuration for image management

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-// Cloudinary configuration
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
